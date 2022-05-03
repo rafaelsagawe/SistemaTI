99fa4fb019c74da67c8ba57038b1e400ed9d1e13
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2022-05-03 17:08:38 -0300

    Combobox de Situação do equipamento

--- SistemaTI/Controllers/HomeController.cs
+++ SistemaTI/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SistemaTI.Controllers
             return View();
         }
 
+        public IActionResult SobreAmbiente()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
